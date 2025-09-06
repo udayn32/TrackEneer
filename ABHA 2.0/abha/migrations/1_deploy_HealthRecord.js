1f39d821f188f78030d168e19d9d0c72235aceca
Uday Naik <udaynaik057@gmail.com>
2025-09-06 23:38:46 +0530

    .

--- ABHA 2.0/abha/migrations/1_deploy_HealthRecord.js
+++
@@ -1,6 +0,0 @@
-const HealthRecord = artifacts.require("HealthRecord");
-
-module.exports = async function(deployer) {
-  // Deploy the contract without constructor arguments as none are required
-  await deployer.deploy(HealthRecord);
-};
